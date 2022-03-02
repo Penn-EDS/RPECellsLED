67a6682774ad7792cad953b2d9a6fc2605c39ff7
quijanop <93285324+quijanop@users.noreply.github.com>
2022-03-02 09:42:34 -0500

    Adding missing void loop
    For some reason the void loop function was missing. Also has the digital pins that are currently used.

--- RPE_LED_Control/RPE_LED_Control.ino
+++ RPE_LED_Control/RPE_LED_Control.ino
@@ -2,11 +2,11 @@
 // of the Arduino Nano
 
 #define LEDArray1 3
-#define LEDArray2 6
+#define LEDArray2 9
 #define Button 2
 #define Pot A0
 int n=0;
-int Values[2]={128,128};
+int Values[2]={128,0};
 void setup() {
   // put your setup code here, to run once:
   //Serial.begin(9600);
@@ -20,4 +20,18 @@ void setup() {
   digitalWrite(LEDArray1, LOW);
   digitalWrite(LEDArray2, LOW);
 
+}
+
+void loop() {
+ analogWrite(LEDArray1,Values[0]);
+ analogWrite(LEDArray2,Values[1]);
+ Values[n]=(analogRead(Pot)/4);
+ if(digitalRead(Button)==LOW){
+  while(digitalRead(Button)==LOW){
+  }
+  ++n;
+  if(n>1){
+    n=0;
+  }
+ }
 }
